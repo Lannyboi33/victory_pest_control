65a7af079e2cbb1f8192a542eec5cf24b0d9022e
lannyboi33 <graytech33@gmail.com>
2023-06-24 19:17:03 -0700

    Final push before merge

--- index.js
+++ index.js
@@ -4,16 +4,3 @@ for (let i = 0; i < cards.length; i++) {
         window.location.href = "./html/pests.html#contact";
     });
 }
-
-document.getElementById('scrollToContact').addEventListener('click', function(event) {
-    event.preventDefault();
-    const contactSection = document.getElementById('contact');
-    const sectionHeight = contactSection.offsetHeight;
-    const windowHeight = window.innerHeight;
-    const scrollOffset = contactSection.offsetTop - (windowHeight / 2) + (sectionHeight / 2);
-
-    window.scrollTo({
-        top: scrollOffset,
-        behavior: 'smooth'
-    });
-});
