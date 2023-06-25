2fef0e5fc52836ed71815b0033dd8925ece0e6c7
lannyboi33 <graytech33@gmail.com>
2023-06-24 19:47:59 -0700

    changed bugs to have their own page

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
-const cards = document.querySelectorAll(".card");
-for (let i = 0; i < cards.length; i++) {
-    cards[i].addEventListener("click", function () {
-        window.location.href = "./html/pests.html#contact";
-    });
-}
+const card = document.querySelector("#ant");
+
+card.addEventListener("click", function () {
+    window.location.href = "./html/ant.html";
+})
+
+const card1 = document.querySelector("#termite");
+
+card1.addEventListener("click", function () {
+    window.location.href = "./html/termite.html";
+})
+
+const card2 = document.querySelector("#rodent");
+
+card2.addEventListener("click", function () {
+    window.location.href = "./html/rodent.html";
+})
+
+const card3 = document.querySelector("#boxbeetle");
+
+card3.addEventListener("click", function () {
+    window.location.href = "./html/boxbeetle.html";
+})
+
+const card4 = document.querySelector("#bedbug");
+
+card4.addEventListener("click", function () {
+    window.location.href = "./html/bedbug.html";
+})
+
+const card5 = document.querySelector("#wasp");
+
+card5.addEventListener("click", function () {
+    window.location.href = "./html/wasp.html";
+})
+
+const card6 = document.querySelector("#spider");
+
+card6.addEventListener("click", function () {
+    window.location.href = "./html/spider.html";
+})
