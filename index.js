a2c5e539fbf10e538cbe6bdad5c31a5e6607856f
lannyboi33 <graytech33@gmail.com>
2023-06-24 18:29:23 -0700

    Test og meta tags

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const cards = document.querySelectorAll(".card");
 for (let i = 0; i < cards.length; i++) {
     cards[i].addEventListener("click", function () {
-        window.location.href = "./html/pests.html#bugs";
+        window.location.href = "./html/pests.html#contact";
     });
 }
+
+document.getElementById('scrollToContact').addEventListener('click', function(event) {
+    event.preventDefault();
+    const contactSection = document.getElementById('contact');
+    const sectionHeight = contactSection.offsetHeight;
+    const windowHeight = window.innerHeight;
+    const scrollOffset = contactSection.offsetTop - (windowHeight / 2) + (sectionHeight / 2);
+
+    window.scrollTo({
+        top: scrollOffset,
+        behavior: 'smooth'
+    });
+});
