a594abed5df6bc986b27294ed4c11a2589546821
lannyboi33 <graytech33@gmail.com>
2023-08-21 13:57:51 -0700

    all bug images resized
    Resized bug images for faster loading time

--- Data/Pests.js
+++ Data/Pests.js
@@ -2,38 +2,38 @@ const Pests =
 [
     {
         title: "ANTS",
-        img: "../images/bugs/ant.jpg",
+        img: "../images/bugs/ant_small.jpg",
         altText: "A picture of ants",
         description: "We contend with a variety of ants in Lane County, such as odorous house ants, velvety tree ants and carpenter ants. When you choose Victory pest control, we will use an integrated pest management approach, to identify and properly treat the ants that are invading your home.",
     },
     {
         title: "TERMITES",
-        img: "../images/bugs/termite.jpg",
+        img: "../images/bugs/termite_small.jpg",
         description: "Termites cause millions of dollars in damage to homes every year. Victory Pest Control provides a free termite inspection, and show you affordable solutions. Not only to protect against termites but other wood destroying organisms as well.",
     },
     {
         title: "RODENTS",
-        img: "../images/bugs/rat.jpg",
+        img: "../images/bugs/rat_small.jpg",
         description: "Mice and rats have become a major problem in the Lane County. Victory Pest Control eliminates rodents from your home, and seals off entry points so they can't get back in.",
     },
     {
         title: "BOXELDER BEETLES",
-        img: "../images/bugs/stinkbug.jpg",
+        img: "../images/bugs/stinkbug_small.jpg",
         description: "Boxelder beetles are a major nuisances. Certain times of year they will congregate in large numbers on the warm sunny side of your home. This often leads to them invading your home through windows and other openings. Victory Pest Control provides a thorough treatment in and around your home, building a barrier to keep theses and other Pest out of your home.",
     },
     {
         title: "BEDBUGS",
-        img: "../images/bugs/bedbug.jpg",
+        img: "../images/bugs/bedbug_small.jpg",
         description: "Sleep tight don't let the bedbugs bite. Most of us have heard this old adage, but did you know bedbug infestations have become very prevalent in the last few years. Our trained experts at Victory pest control believe in an extremely thorough and full treatment for bedbugs. We also back up our treatment with a warranty, so we know your family can sleep in peace at night.",
     },
     {
         title: "WASPS/HORNETS",
-        img: "../images/bugs/wasp.jpg",
+        img: "../images/bugs/wasp_small.jpg",
         description: "Wasp and hornets can cause alot of worry for people, especially if they are allergic to their stings. When you call Victory pest control we will not only spray and kill them but we will also remove their nest.",
     },
     {
         title: "SPIDERS",
-        img: "../images/bugs/spider.jpg",
+        img: "../images/bugs/spider_small.jpg",
         description: "At Victory pest control we understand many spiders are beneficial to our environment. Although sometimes it can be difficult to tell what is a good spider compared to a dangerous spider. Keeping a barrier around your home will eliminate the worry of what type of spiders are lurking around your home.",
     }
 ]
